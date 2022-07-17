ffdd4964aea05c99fc485484262904599def8748
Rajkumar <108941795+Rajedyoda@users.noreply.github.com>
2022-07-17 13:54:01 +0530

    Intial commit

---
+++ Fibonacci_series.py
@@ -0,0 +1,18 @@
+
+
+print("Fibonacci series between 0 to 50 is :")
+fib1 = 0
+fib2 = 1
+print(fib1)
+while fib2<50:
+    print(fib2)
+    fib3 = fib1+fib2
+    fib1 = fib2
+    fib2 = fib3
+
+
+
+
+
+
+
