0ce89ce7c71114200475f4670528aacd3b4ca855
Rajkumar <108941795+Rajedyoda@users.noreply.github.com>
2022-07-17 14:52:36 +0530

    Count of Even and odd in the list

---
+++ count_even_and_odd.py
@@ -0,0 +1,23 @@
+print("The Count of even and odd in the given list is : ")
+print()
+
+lst = [1,2,3,4,5,6,7,8,9]
+ 
+even_count = 0
+odd_count = 0
+ 
+for num in lst:
+     
+    if num % 2 == 0:
+        even_count += 1
+ 
+    else:
+        odd_count += 1
+         
+print("Even numbers in the list: ", even_count)
+print("Odd numbers in the list: ", odd_count)
+
+
+
+
+
